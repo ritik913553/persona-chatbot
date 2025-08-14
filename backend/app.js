4a021ae9c1acfc55c1074b23b1d593ff6214f224
ritik913553 <ritikgupta9135@gmail.com>
2025-08-15 00:32:23 +0530

    Code for Deployment

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,10 @@
-import express from 'express';
-import 'dotenv/config';
-import { getChat } from './controllers/chat.controller.js';
-import cors from 'cors';
+import express from "express";
+import "dotenv/config";
+import { getChat } from "./controllers/chat.controller.js";
+import cors from "cors";
+import path from "path";
+import fs from "fs";
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,14 +13,20 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
-
-
 // Routes
-app.post('/api/chat', getChat);
+app.post("/api/chat", getChat);
+
+if (process.env.NODE_ENV === "production") {
+    const dirPath = path.resolve();
+    app.use(express.static("./frontend/dist"));
+    app.get("", (req, res) => {
+        res.sendFile(path.resolve(dirPath, "./frontend/dist", "index.html"));
+    });
+}
+
 
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
-
